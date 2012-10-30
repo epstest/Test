532d24683f33da6ab8de17714177b3342c5fce2a
dominic <dominic@System-44>
2012-10-30 18:23:54 +0100

    EPS Test DVT

---
+++ JavaApplication3/src/javaapplication3/JavaApplication3.java
@@ -0,0 +1,27 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication3;
+
+import java.io.*;
+
+/**
+ *
+ * @author dominic
+ */
+public class JavaApplication3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) throws IOException {
+        // TODO code application logic here
+        BufferedReader input = new BufferedReader(new InputStreamReader(
+                System.in));
+        for (int i = 1; i < 10; i++) {
+            System.out.println("HELLO");
+            
+        }
+    }
+}
