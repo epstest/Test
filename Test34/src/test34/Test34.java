2310dbd017c423c62630f20ab6951c3b28d4fb52
dominic <dominic@System-44>
2012-10-30 19:16:33 +0100

    EPS Test DVT hisafd

---
+++ Test34/src/test34/Test34.java
@@ -0,0 +1,20 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package test34;
+
+/**
+ *
+ * @author dominic
+ */
+public class Test34 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.print("Hallo Leute");
+    }
+}
